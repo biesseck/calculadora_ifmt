917eead864de2fa780b5bf210646119e87669f8e
taylla <tayllagarcia1023@gmail.com>
2022-06-15 17:34:09 -0400

    Auto stash before merge of "main" and "origin/main"
    Add mensagem de taylla

--- main.js
+++ main.js
@@ -2,10 +2,21 @@ Olá pessoal, aqui é o prof Bernardo!
 
 Eu sou o Victor e estou no comando!...
 
+<<<<<<< Updated upstream
 ta o comando porra nenhuma kkkkkkkkkkkk 
 
 Sou Emily 
+<<<<<<< Updated upstream
 
 Oi me chamo leticia
 
 Bernardo Biesseck
+=======
+=======
+<<<<<<< Updated upstream
+ta o comando porra nenhuma kkkkkkkkkkkk 
+=======
+  
+  
+  eu me chamo taylla
+>>>>>>> Stashed changes
